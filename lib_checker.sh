6b1a73453c41959d7cec9c867252b9d7e79510ca
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-06-21 15:48:54 +0200

    Modify shebang

--- lib_checker.sh
+++ lib_checker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Functions in project
 libc="memset bzero memcpy memccpy memmove memchr memcmp strlen strdup strcpy strncpy strcat strncat strlcat strchr strrchr strstr strnstr strcmp strncmp atoi isalpha isdigit isalnum isascii isprint toupper tolower"
